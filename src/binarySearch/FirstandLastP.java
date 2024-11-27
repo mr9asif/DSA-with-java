648ff717231b4152ba49daad73dee8f81e44aa1a
mr9 asif <asifalibd002@gmail.com>
2024-11-27 14:31:42 +0600

    added infinite array

--- src/binarySearch/FirstandLastP.java
+++ src/binarySearch/FirstandLastP.java
@@ -1,32 +1,46 @@
 package binarySearch;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class FirstandLastP {
     public static void main(String[] args) {
-        int[] arr = {2,3,5,6,8,9,12,13,15};
-        int target = 6;
-        int ans = firstAndLastP(arr, target);
-        int[] f = {ans-1, ans +1};
-        System.out.println(Arrays.toString(f));
+        int[] nums = {2, 3, 4, 5, 5, 5, 5, 7, 8, 9}; // Sorted array
+        int target = 5; // The target to find
+        int[] ans = searchRange(nums, target);
+        System.out.println("Target: " + target + " -> Result: " + Arrays.toString(ans));
     }
-    static  int firstAndLastP(int[] arr, int target){
-         int start = 0;
-         int end = arr.length -1;
 
-         while(start <= end){
-             int mid = start + (end - start) / 2;
+    static int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+        // Find the first occurrence
+        ans[0] = search(nums, target, true);
+        // Find the last occurrence
+        ans[1] = search(nums, target, false);
+        return ans;
+    }
 
-             if(target < arr[mid]){
-                 end = mid -1;
-             }else if(target > arr[mid]){
-                 start = mid + 1;
-             }else{
-                 return mid;
-             }
-         }
-         return  -1;
+    static int search(int[] nums, int target, boolean findStartIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
 
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                // Target is found
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1; // Move left to find the first occurrence
+                } else {
+                    start = mid + 1; // Move right to find the last occurrence
+                }
+            }
+        }
+        return ans;
     }
 }
