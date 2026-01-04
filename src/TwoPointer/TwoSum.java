0fe071603046a0905869ebe384c151d3e17392d0
mr9 asif <asifalibd002@gmail.com>
2026-01-04 14:22:28 +0600

    added two sum

--- src/TwoPointer/TwoSum.java
+++ src/TwoPointer/TwoSum.java
@@ -1,5 +1,29 @@
 package TwoPointer;
 
 public class TwoSum {
-    
+     public static void main(String[] args) {
+       int[] arr = {2,3,5,6,8,9};
+       int target = 7;
+       int i=0;
+       int j=arr.length-1;
+       boolean found=false;
+       while(i<j){
+           int sum = arr[i]+arr[j];
+           if(sum == target){
+               System.out.println("found target: " + target);
+               found=true;
+               break;
+               
+           }
+           if(sum < target){
+               i++;
+           }else{
+               j--;
+           }
+       }
+       if(!found){
+             System.out.println("not foundd");
+       }
+        
+    }
 }
