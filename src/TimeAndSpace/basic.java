04a8f8c2fd519e3ec97edab0eba736ee865cf442
mr9 asif <asifalibd002@gmail.com>
2026-02-06 23:04:02 +0600

    add

--- src/TimeAndSpace/basic.java
+++ src/TimeAndSpace/basic.java
@@ -16,7 +16,36 @@ public class basic {
     //  O(m+n)
     }
     */
-    
+
+    // constand time complexity
+
+    /* 
+    1.for(int i=1; i<100; i++){
+       System.out.println("asif");
+    }
+       O(1)  // here time complexity ,,constant
+
+
+     2. for(int i=1; i<=n; i*=2){
+       System.out.println("asif");
+     }
+      O(log(n));
+
+      3. for(int i=1; i<=n; i*=2){
+           for(int j=1; j<=n; j++){
+            sout("asof");
+           }
+      }
+     O(nlog(n));
+
+     4. for(int i=1; i*i<=n; i++){
+          sout("asif");
+     }
+          O(Sqrt(n))
+    */
+    // asdfdssdfdsf
+
+
     
     
 }
